569635893f3b4849e6ad3df6ff3c2ceeb903e700
Peanut44 <Peanutmakunat77@gmail.com>
2026-03-30 22:13:04 -0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,16 +1,16 @@
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+  event.respondWith(handleRequest(event.request));
+});
 
 async function handleRequest(request) {
-  const targetURL = 'https://live20.bozztv.com/giatvplayout7/giatv-210631/tracks-v1a1/mono.ts.m3u8'; // Example URL, replace with your actual stream or resource
+  // I-check kung ang request ay para sa 'mono.ts.m3u8'
+  const url = new URL(request.url);
+  if (url.pathname.endsWith('mono.ts.m3u8')) {
+    // Kung oo, i-redirect sa bagong URL
+    const newUrl = 'https://live20.bozztv.com/giatvplayout7/giatv-210631/tracks-v1a1/mono.ts.m3u8';
+    return Response.redirect(newUrl, 302);
+  }
 
-  const modifiedRequest = new Request(targetURL, {
-    method: request.method,
-    headers: request.headers,
-    body: request.body
-  })
-
-  const response = await fetch(modifiedRequest)
-  return response
+  // Kung hindi, ipasa lang ang request
+  return fetch(request);
 }
