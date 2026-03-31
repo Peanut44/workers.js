174f684267f126993e4387296e2f3baa8427eb7f
Peanut44 <Peanutmakunat77@gmail.com>
2026-03-30 21:55:41 -0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,19 +3,14 @@ addEventListener('fetch', event => {
 })
 
 async function handleRequest(request) {
-  // URL ng iyong original stream
-  const targetURL = 'https://live20.bozztv.com/giatvplayout7/giatv-210631/tracks-v1a1/mono.ts.m3u8'
+  const targetURL = 'https://live20.bozztv.com/giatvplayout7/giatv-210631/tracks-v1a1/mono.ts.m3u8'; // Example URL, replace with your actual stream or resource
 
-  // Gumawa ng bagong request sa target na URL
   const modifiedRequest = new Request(targetURL, {
     method: request.method,
     headers: request.headers,
     body: request.body
   })
 
-  // Kunin ang response mula sa target stream
   const response = await fetch(modifiedRequest)
-
-  // I-serve ang response pabalik sa user (proxy ng stream)
   return response
 }
