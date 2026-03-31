5f9a0fab272132cefcfe38abd3052b14432e8810
Peanut44 <Peanutmakunat77@gmail.com>
2026-03-30 22:30:30 -0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,16 +1,7 @@
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request));
+  event.respondWith(
+    new Response('This is a test worker!', {
+      headers: { 'content-type': 'text/plain' }
+    })
+  );
 });
-
-async function handleRequest(request) {
-  // I-check kung ang request ay para sa 'mono.ts.m3u8'
-  const url = new URL(request.url);
-  if (url.pathname.endsWith('mono.ts.m3u8')) {
-    // Kung oo, i-redirect sa bagong URL
-    const newUrl = 'https://live20.bozztv.com/giatvplayout7/giatv-210631/tracks-v1a1/mono.ts.m3u8';
-    return Response.redirect(newUrl, 302);
-  }
-
-  // Kung hindi, ipasa lang ang request
-  return fetch(request);
-}
